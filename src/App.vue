fc5d4f256fd3fd393f7dd3c9f8dc6d1ad1f103de
ziad abosamra <aboamraziyad@gmail.com>
2026-09-14 15:14:10 +0200

    chore: initial boilerplate setup

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header>
+    <!-- حط هنا المكون الخاص بالـ Navbar مستقبلاً -->
+  </header>
+
+  <main>
+    <RouterView />
+  </main>
+</template>
+
+<style scoped>
+/* الستايل الخاص بالـ Layout الرئيسي لو محتاج */
+</style>
